062960f5e0234ad40c3ebd27d9f90a8e2041b469
AnnemarieFulcer <aef22@njit.edu>
2018-06-12 22:01:09 -0400

    setup website nav

---
+++ resources/views/layout/default.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en">
+@include ('includes.head')
+<body>
+@include ('includes.menu')
+@yield ('content')
+
+@include ('includes.jsfooter')
+</body>
+</html>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.default')
+@section ('content')
+
+@include ('includes.head')
+<body>
+
+<h1>Hello, world!</h1>
+
+@endsection
